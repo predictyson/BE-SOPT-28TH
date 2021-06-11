0a4c7d246ac69d818109d0b0bf3d76aeefb54774
yezgoget <yejison98@gmail.com>
2021-06-11 20:55:10 +0900

    step10: diary view

---
+++ 7th/diary-app/src/App.js
@@ -0,0 +1,48 @@
+import React , {useState} from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import MainHeader from "./components/common/MainHeader";
+import Calendar from "./components/common/Calendar";
+import Title from "./components/common/Title";
+import Footer from "./components/common/Footer";
+import Main from "./pages/Main";
+import Diary from "./pages/Diary";
+
+const getCurrentDate = () => {
+  const now = new Date();
+  const currentYear = now.getFullYear();
+  const currentMonth = now.getMonth();
+  return { year: currentYear, month: currentMonth};
+};
+
+function App() {
+  const [year, setYear] = useState(getCurrentDate().year);
+  const [month, setMonth] = useState(getCurrentDate().month);
+  return (
+    <>
+   <BrowserRouter>
+   <MainHeader />
+    <Calendar 
+    currentYear={year}
+    setCurrentYear={setYear}
+    currentMonth={month}
+    setCurrentMonth={setMonth}
+    />
+    <Title />
+   <Switch>
+   <Route
+            exact
+            path="/"
+            component={() => <Main year={year} month={month} />}
+          />
+          <Route 
+          path="/diary/edit/:id"
+          component={() => <Diary year={year} month={month} />}
+          />
+   </Switch>
+   </BrowserRouter>
+   <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ 7th/diary-app/src/pages/Main.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import Card from "../components/main/Card";
+import NewCard from "../components/main/NewCard";
+import Styled from "styled-components";
+import { getCardData } from "../lib/api";
+import { withRouter } from "react-router-dom";
+
+const MainWrap = Styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, auto);
+  row-gap: 25px;
+`;
+
+const Main = ({year, month, history}) => {
+    const [userData, setUserData] = React.useState(null);
+    const [rawData, setRawData] = React.useState(null);
+
+    React.useEffect(() => {
+        (async () =>{
+            const data = await getCardData();
+            setRawData(data);
+            data[year] && setUserData(data[year][month]);
+        })();
+    },[year, month]);
+        return (
+            <MainWrap>
+                   {userData &&
+        userData.map((data, index) => {
+          return (
+            <Card
+              key={index}
+              props={data}
+              onClickFunc={() => history.push(`/diary/${data.id}`)}
+            />
+          );
+        })}
+          <NewCard
+        year={year}
+        month={month}
+        rawData={rawData}
+        setUserData={setUserData}
+        id={userData ? userData.length + 1 : 1}
+      />
+            </MainWrap>
+    );
+};
+
+export default withRouter(Main);
